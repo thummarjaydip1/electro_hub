a6808c15bd1599194e85d2078f9edac429225479
Thummar <thummarjaydip1@gmail.com>
2026-05-27 10:36:22 +0530

    fixed static serving

--- admin_panel/settings.py
+++ admin_panel/settings.py
@@ -135,6 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "management/static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
