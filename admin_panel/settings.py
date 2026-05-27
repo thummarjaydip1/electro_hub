b6076526d567170e0bbe736f9dd8664226e9d59d
Thummar <thummarjaydip1@gmail.com>
2026-05-27 16:27:53 +0530

    fix whitenoise error

--- admin_panel/settings.py
+++ admin_panel/settings.py
@@ -158,7 +158,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
