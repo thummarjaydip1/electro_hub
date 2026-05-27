e452d47c7214d71dbd24790c18d84875679c8049
Thummar <thummarjaydip1@gmail.com>
2026-05-27 09:45:03 +0530

    changeses apply

--- admin_panel/settings.py
+++ admin_panel/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # live server ma run karva mate
 
 
 # Application definition
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'management/static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
